f819aee18ae251b7ce9a5e19508558561ce50dcb
Ben Rowe <ben.rowe.83@gmail.com>
2025-11-28 16:47:28 +1100

    Add basic methods to state flow for transition

--- src/StateFlow.php
+++ src/StateFlow.php
@@ -12,4 +12,12 @@ class StateFlow
     public function __construct()
     {
     }
+
+    /**
+     * @param array<string, mixed> $array
+     */
+    public function transition(State $param, array $array): StateWorker
+    {
+        return new StateWorker();
+    }
 }
