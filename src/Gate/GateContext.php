da97f586ffa7699186c4f7dd7b0fc19f5e7e4726
Ben Rowe <ben.rowe.83@gmail.com>
2025-11-29 09:50:22 +1100

    Fix issues with gate context

--- src/Gate/GateContext.php
+++ src/Gate/GateContext.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace BenRowe\StateFlow\Gate;
 
+use BenRowe\StateFlow\State;
+
 readonly class GateContext
 {
+    /**
+     * @param array<string, mixed> $desiredState
+     */
     public function __construct(public State $currentState, public array $desiredState)
     {
     }
