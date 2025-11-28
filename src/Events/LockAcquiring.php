2ca601b818a28ea17fcf3f074c91573f1b315224
Ben Rowe <ben.rowe.83@gmail.com>
2025-11-28 22:15:28 +1100

    Add event LockAcquiring

---
+++ src/Events/LockAcquiring.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BenRowe\StateFlow\Events;
+
+use BenRowe\StateFlow\State;
+
+class LockAcquiring extends Event
+{
+    public function __construct(
+        public string $lockKey,
+        public State $state,
+    ) {
+        parent::__construct();
+    }
+}
