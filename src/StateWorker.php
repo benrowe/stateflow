3ab95ea37eef19e2f5a8c3648308e4c0fe9ddf47
Ben Rowe <ben.rowe.83@gmail.com>
2025-11-28 16:47:28 +1100

    Add basic methods to state flow for transition

---
+++ src/StateWorker.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BenRowe\StateFlow;
+
+class StateWorker
+{
+}
--- tests/StateFlowTest.php
+++ tests/StateFlowTest.php
@@ -4,15 +4,18 @@ declare(strict_types=1);
 
 namespace BenRowe\StateFlow\Tests;
 
+use BenRowe\StateFlow\State;
 use BenRowe\StateFlow\StateFlow;
+use BenRowe\StateFlow\StateWorker;
 use PHPUnit\Framework\TestCase;
 
 class StateFlowTest extends TestCase
 {
-    public function test_it_can_be_created_with_initial_state(): void
+    public function testItCanBeInitialised(): void
     {
         $stateFlow = new StateFlow();
 
         $this->assertInstanceOf(StateFlow::class, $stateFlow);
+        $this->assertInstanceOf(StateWorker::class, $stateFlow->transition(new class () implements State {}, []));
     }
 }
