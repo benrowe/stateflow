0ae576a87e022967adb92e2537e92b28abd27242
Ben Rowe <ben.rowe.83@gmail.com>
2025-11-29 11:02:18 +1100

    Add configuration class + unit test

---
+++ src/Configuration/Configuration.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BenRowe\StateFlow\Configuration;
+
+use BenRowe\StateFlow\Action\Action;
+use BenRowe\StateFlow\Gate\Gate;
+
+readonly class Configuration
+{
+    /**
+     * @param Gate[] $transitionGates
+     * @param Action[] $actions
+     */
+    public function __construct(private array $transitionGates, private array $actions)
+    {
+
+    }
+
+    /**
+     * @return Gate[]
+     */
+    public function getTransitionGates(): array
+    {
+        return $this->transitionGates;
+    }
+
+    /**
+     * @return Action[]
+     */
+    public function getActions(): array
+    {
+        return $this->actions;
+    }
+
+}
