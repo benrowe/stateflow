57d1d5ee47e3b54610c5de87cbecbd27e11c7968
Ben Rowe <ben.rowe.83@gmail.com>
2025-11-29 11:58:20 +1100

    Get very rough implementation of worker running

--- src/StateFlow.php
+++ src/StateFlow.php
@@ -4,20 +4,40 @@ declare(strict_types=1);
 
 namespace BenRowe\StateFlow;
 
+use BenRowe\StateFlow\Configuration\CallableConfigurationProvider;
+use BenRowe\StateFlow\Configuration\Configuration;
+use BenRowe\StateFlow\Configuration\ConfigurationProvider;
+use Closure;
+
 /**
  * StateFlow - A flexible state machine implementation
  */
 class StateFlow
 {
-    public function __construct()
+    public function __construct(private Closure|ConfigurationProvider $configProvider)
     {
     }
 
     /**
-     * @param array<string, mixed> $array
+     * @param array<string, mixed> $delta
      */
-    public function transition(State $param, array $array): StateWorker
+    public function transition(State $currentState, array $delta): StateWorker
     {
-        return new StateWorker();
+        $context = new TransitionContext($currentState);
+
+        return new StateWorker($context, $this->resolveConfig($currentState, $delta));
+    }
+
+    /**
+     * @param array<string, mixed> $delta
+     */
+    private function resolveConfig(State $currentState, array $delta): Configuration
+    {
+        $provider = $this->configProvider;
+        if ($provider instanceof Closure) {
+            $provider = new CallableConfigurationProvider($provider);
+        }
+
+        return $provider->provide($currentState, $delta);
     }
 }
