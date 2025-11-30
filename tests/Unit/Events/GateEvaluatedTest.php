ba6a7de0ce67eda4e27d17d27d4111a100eb1923
Ben Rowe <ben.rowe.83@gmail.com>
2025-12-01 06:20:22 +1100

    Add unit tests for gate eval events

---
+++ tests/Unit/Events/GateEvaluatedTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BenRowe\StateFlow\Tests\Unit\Events;
+
+use BenRowe\StateFlow\Events\GateEvaluated;
+use BenRowe\StateFlow\Gate\Gate;
+use BenRowe\StateFlow\Gate\GateContext;
+use BenRowe\StateFlow\Gate\GateResult;
+use BenRowe\StateFlow\State;
+use PHPUnit\Framework\TestCase;
+
+class GateEvaluatedTest extends TestCase
+{
+    public function testGetters(): void
+    {
+        $gate = $this->createMock(Gate::class);
+        $context = new GateContext(
+            $this->createMock(State::class),
+            ['foo' => 'bar']
+        );
+        $result = GateResult::ALLOW;
+        $isActionGate = true;
+
+        $event = new GateEvaluated($gate, $context, $result, $isActionGate);
+
+        $this->assertSame($gate, $event->gate);
+        $this->assertSame($context, $event->context);
+        $this->assertSame($result, $event->result);
+        $this->assertSame($isActionGate, $event->isActionGate);
+    }
+}
