7d9261236a4d77f380f46d9ca144cc38ec5f5703
Ben Rowe <ben.rowe.83@gmail.com>
2025-12-22 14:15:17 +1100

    Relocate config array logic to factory

---
+++ src/Configuration/ConfigurationFactory.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BenRowe\StateFlow\Configuration;
+
+use BenRowe\StateFlow\Action\Action;
+use BenRowe\StateFlow\Action\ActionCollection;
+use BenRowe\StateFlow\Exceptions\InvalidConfigurationException;
+use BenRowe\StateFlow\Gate\Gate;
+use BenRowe\StateFlow\Gate\GateCollection;
+
+class ConfigurationFactory
+{
+    /**
+     * @param Gate[] $transitionGates
+     * @param Action[] $actions
+     */
+    public function makeFromArray(array $transitionGates, array $actions): Configuration
+    {
+        return new Configuration(
+            $this->createGateCollection($transitionGates),
+            $this->createActionCollection($actions)
+        );
+    }
+
+    /**
+     * @param Gate[] $gates
+     */
+    private function createGateCollection(array $gates): GateCollection
+    {
+        $this->validateGates($gates);
+
+        return GateCollection::fromArray($gates);
+    }
+
+    /**
+     * @param Action[] $actions
+     */
+    private function createActionCollection(array $actions): ActionCollection
+    {
+        $this->validateActions($actions);
+
+        return ActionCollection::fromArray($actions);
+    }
+
+    /**
+     * @param array<mixed> $gates
+     * @phpstan-assert Gate[] $gates
+     */
+    private function validateGates(array $gates): void
+    {
+        foreach ($gates as $index => $gate) {
+            if (!$gate instanceof Gate) {
+                throw new InvalidConfigurationException(
+                    sprintf(
+                        'Gate at index %d must implement %s, %s given',
+                        $index,
+                        Gate::class,
+                        get_debug_type($gate)
+                    )
+                );
+            }
+        }
+    }
+
+    /**
+     * @param array<mixed> $actions
+     * @phpstan-assert Action[] $actions
+     */
+    private function validateActions(array $actions): void
+    {
+        foreach ($actions as $index => $action) {
+            if (!$action instanceof Action) {
+                throw new InvalidConfigurationException(
+                    sprintf(
+                        'Action at index %d must implement %s, %s given',
+                        $index,
+                        Action::class,
+                        get_debug_type($action)
+                    )
+                );
+            }
+        }
+    }
+}
